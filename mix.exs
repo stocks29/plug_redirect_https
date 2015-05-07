ce5746d12d48c0889c6e8f33d5f842b97e04e042
Bob Stockdale <bstockdale@zappos.com>
2015-05-07 10:19:46 -0400

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PlugRedirectHttps.Mixfile do
 
   def project do
     [app: :plug_redirect_https,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.0",
      name: "plug_redirect_https",
      source_url: "https://github.com/stocks29/plug_redirect_https",
